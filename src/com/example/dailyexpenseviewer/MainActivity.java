1cd0e7de0cf5a358892a40fc5a8b6ae15baa4c55
Fatih Ozturk <kandirali77@gmail.com>
2014-04-26 11:29:02 +0200

    Build new GUI

--- src/com/example/dailyexpenseviewer/MainActivity.java
+++ src/com/example/dailyexpenseviewer/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.dailyexpenseviewer;
 
+import java.util.Calendar;
+
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.DatePicker;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -60,6 +64,24 @@ public class MainActivity extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
+    }
+    
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    	 @Override
+    	    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    	        // Use the current date as the default date in the picker
+    	        final Calendar c = Calendar.getInstance();
+    	        int year = c.get(Calendar.YEAR);
+    	        int month = c.get(Calendar.MONTH);
+    	        int day = c.get(Calendar.DAY_OF_MONTH);
+
+    	        // Create a new instance of DatePickerDialog and return it
+    	        return new DatePickerDialog(getActivity(), this, year, month, day);
+    	    }
+
+    	    public void onDateSet(DatePicker view, int year, int month, int day) {
+    	        // Do something with the date chosen by the user
+    	    }
     }
 
 }
